bd9d7000c2f8af9649d3c5d21e2231f3c7919d2d
JateenTirlotkar <jateentirlotkar11@gmail.com>
2022-04-17 13:54:06 +0530

    initailcommit

--- base/views.py
+++ base/views.py
@@ -7,13 +7,6 @@ from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic, Message, User
 from .forms import RoomForm, UserForm, MyUserCreationForm
 
-# Create your views here.
-
-# rooms = [
-#     {'id': 1, 'name': 'Lets learn python!'},
-#     {'id': 2, 'name': 'Design with me'},
-#     {'id': 3, 'name': 'Frontend developers'},
-# ]
 
 
 def loginPage(request):
@@ -51,6 +44,7 @@ def registerPage(request):
     form = MyUserCreationForm()
 
     if request.method == 'POST':
+        #moodle = request.POST.get('moodle id')
         form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
